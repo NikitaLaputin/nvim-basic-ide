16667c72fccd8b2c355d8b9c6ecfbd70623090dd
Mikita Laputsin <mlaputsin@eisgroup.com>
2022-11-14 18:02:16 +0200

    Added semantic highlighting

---
+++ lua/config/semantictokens.lua
@@ -0,0 +1,68 @@
+local M = {}
+
+local set_hl = vim.api.nvim_set_hl
+
+local colors = {
+    purple = '#795DA3',
+    blue = '#0086B3',
+    teal = '#138E8D',
+    light_blue = '#20999D',
+    dark_red = '#912841',
+    deep_blue = '#183691',
+    black = '#333333',
+    green = '#597A2B',
+    swamp_green = '#5F8C59',
+    ocean = '#007B81',
+}
+
+function M.setup()
+  -- token
+  -- https://github.com/nvim-treesitter/nvim-treesitter/blob/master/lua/nvim-treesitter/highlight.lua
+  set_hl(0, "LspParameter", { fg = colors.blue })
+  set_hl(0, "LspType", { fg = colors.green })
+  set_hl(0, "LspClass", { fg = colors.teal })
+  -- set_hl(0, "LspComment", { fg = "TSComment" })
+  set_hl(0, "LspDecorator", { fg = colors.black })
+  set_hl(0, "LspEnum", { fg = colors.teal })
+  set_hl(0, "LspEnumMember", { fg = colors.light_blue, italic = true })
+  -- set_hl(0, "LspEvent", { fg = "TSProperty" })
+  set_hl(0, "LspFunction", { fg = colors.black, italic = true })
+  set_hl(0, "LspInterface", { fg = colors.green })
+  set_hl(0, "LspKeyword", { fg = colors.dark_red, italic = true })
+  -- set_hl(0, "LspMacro", { fg = "TSFuncMacro" })
+  set_hl(0, "LspMethod", { fg = colors.purple })
+  -- set_hl(0, "LspModifier", { fg = "TSTypeQualifier" })
+  set_hl(0, "LspNamespace", { fg = colors.purple })
+  set_hl(0, "LspNumber", { fg = colors.light_blue })
+  set_hl(0, "LspOperator", { fg = colors.black, italic = true })
+  set_hl(0, "LspProperty", { fg = colors.light_blue })
+  -- set_hl(0, "LspRegexp", { fg = "TSStringRegex" })
+  set_hl(0, "LspString", { fg = colors.blue })
+  -- set_hl(0, "LspStruct", { fg = "TSTypeDefinition" })
+  set_hl(0, "LspTypeParameter", { fg = colors.teal })
+  set_hl(0, "LspVariable", { fg = colors.light_blue })
+
+  -- modifier
+  -- set_hl(0, "LspDeclaration", { link = "TSDefine" })
+  -- set_hl(0, "LspDefinition", { link = "TSTypeDefinition" })
+  -- set_hl(0, "LspReadonly", { link = "TSContant" })
+  -- set_hl(0, "LspStatic", { link = "TSConsantMacro" })
+  -- set_hl(0, "LspDeprecated", { link = "TSWarning" })
+  -- set_hl(0, "LspAbstract", { fg = "#9E6162" })
+  -- set_hl(0, "LspAsync", { fg = "#81A35C" })
+  -- set_hl(0, "LspModification", { fg = "#7E5CA3" })
+  -- set_hl(0, "LspDocumentation", { fg = "#ccc0f5" })
+  -- set_hl(0, "LspDefaultLibrary", { fg = "#c99dc1" })
+
+  require("nvim-semantic-tokens").setup {
+    preset = "default",
+    -- highlighters is a list of modules following the interface of nvim-semantic-tokens.table-highlighter or
+    -- function with the signature: highlight_token(ctx, token, highlight) where
+    --        ctx (as defined in :h lsp-handler)
+    --        token  (as defined in :h vim.lsp.semantic_tokens.on_full())
+    --        highlight (a helper function that you can call (also multiple times) with the determined highlight group(s) as the only parameter)
+    highlighters = { require "nvim-semantic-tokens.table-highlighter" },
+  }
+end
+
+return M
--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -16,48 +16,48 @@ if not status_ok then
 end
 
 -- overrides
-local colors = {
-    purple = '#795DA3',
-    blue = '#0086B3',
-    teal = '#138E8D',
-    light_blue = '#20999D',
-    dark_red = '#912841',
-    deep_blue = '#183691',
-    black = '#333333',
-    green = '#597A2B',
-    swamp_green = '#5F8C59',
-    ocean = '#007B81',
-}
-local set_color = vim.api.nvim_set_hl
-set_color(0, '@type', { fg = colors.green })
-set_color(0, '@type.builtin', { fg = colors.dark_red, italic = true })
-set_color(0, '@keyword', { fg = colors.dark_red, italic = true })
-set_color(0, '@keyword.function', { fg = colors.dark_red, italic = true })
-set_color(0, '@keyword.return', { fg = colors.dark_red, italic = true })
-set_color(0, '@property', { fg = colors.blue })
-set_color(0, '@tag', { fg = colors.swamp_green })
-set_color(0, '@tag.attribute', { fg = colors.purple })
-set_color(0, '@variable', { fg = colors.ocean })
-set_color(0, '@constructor', { fg = colors.purple })
-set_color(0, '@string', { fg = colors.deep_blue })
-set_color(0, '@include', { fg = colors.dark_red, italic = true })
-set_color(0, '@parameter', { fg = colors.blue })
-set_color(0, '@constant', { fg = colors.ocean })
-set_color(0, '@number', { fg = colors.ocean })
-set_color(0, '@variable.builtin', { fg = colors.blue, italic = true })
-set_color(0, '@constant.builtin', { fg = colors.dark_red, italic = true })
-set_color(0, '@keyword.operator', { fg = colors.dark_red, italic = true })
-set_color(0, '@exception', { fg = colors.dark_red, italic = true })
-set_color(0, '@boolean', { fg = colors.dark_red, italic = true })
-set_color(0, '@method', { fg = colors.dark_red, italic = true })
-set_color(0, '@method.call', { fg = colors.purple })
-set_color(0, '@punctuation.bracket', { fg = colors.black })
-set_color(0, '@operator', { fg = colors.black })
-set_color(0, '@conditional', { fg = colors.dark_red, italic = true  })
-set_color(0, '@function', { fg = colors.black, italic = true })
-set_color(0, '@function.call', { fg = colors.black, italic = true })
-set_color(0, '@generic', { fg = colors.light_blue })
-set_color(0, '@decorator', { fg = colors.black })
-set_color(0, '@method', { fg = colors.purple })
-set_color(0, '@class', { fg = colors.teal })
-set_color(0, '@jsx.tag', { fg = colors.green })
+-- local colors = {
+--     purple = '#795DA3',
+--     blue = '#0086B3',
+--     teal = '#138E8D',
+--     light_blue = '#20999D',
+--     dark_red = '#912841',
+--     deep_blue = '#183691',
+--     black = '#333333',
+--     green = '#597A2B',
+--     swamp_green = '#5F8C59',
+--     ocean = '#007B81',
+-- }
+-- local set_color = vim.api.nvim_set_hl
+-- set_color(0, '@type', { fg = colors.green })
+-- set_color(0, '@type.builtin', { fg = colors.dark_red, italic = true })
+-- set_color(0, '@keyword', { fg = colors.dark_red, italic = true })
+-- set_color(0, '@keyword.function', { fg = colors.dark_red, italic = true })
+-- set_color(0, '@keyword.return', { fg = colors.dark_red, italic = true })
+-- set_color(0, '@property', { fg = colors.blue })
+-- set_color(0, '@tag', { fg = colors.swamp_green })
+-- set_color(0, '@tag.attribute', { fg = colors.purple })
+-- set_color(0, '@variable', { fg = colors.ocean })
+-- set_color(0, '@constructor', { fg = colors.purple })
+-- set_color(0, '@string', { fg = colors.deep_blue })
+-- set_color(0, '@include', { fg = colors.dark_red, italic = true })
+-- set_color(0, '@parameter', { fg = colors.blue })
+-- set_color(0, '@constant', { fg = colors.ocean })
+-- set_color(0, '@number', { fg = colors.ocean })
+-- set_color(0, '@variable.builtin', { fg = colors.blue, italic = true })
+-- set_color(0, '@constant.builtin', { fg = colors.dark_red, italic = true })
+-- set_color(0, '@keyword.operator', { fg = colors.dark_red, italic = true })
+-- set_color(0, '@exception', { fg = colors.dark_red, italic = true })
+-- set_color(0, '@boolean', { fg = colors.dark_red, italic = true })
+-- set_color(0, '@method', { fg = colors.dark_red, italic = true })
+-- set_color(0, '@method.call', { fg = colors.purple })
+-- set_color(0, '@punctuation.bracket', { fg = colors.black })
+-- set_color(0, '@operator', { fg = colors.black })
+-- set_color(0, '@conditional', { fg = colors.dark_red, italic = true  })
+-- set_color(0, '@function', { fg = colors.black, italic = true })
+-- set_color(0, '@function.call', { fg = colors.black, italic = true })
+-- set_color(0, '@generic', { fg = colors.light_blue })
+-- set_color(0, '@decorator', { fg = colors.black })
+-- set_color(0, '@method', { fg = colors.purple })
+-- set_color(0, '@class', { fg = colors.teal })
+-- set_color(0, '@jsx.tag', { fg = colors.green })
