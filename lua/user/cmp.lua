e587c79fa88532b90314c9c198335b4f7dbb2f94
Mikita <mlaputsin@eisgroup.com>
2024-11-12 17:39:41 +0200

    Formatting fix

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -90,21 +90,6 @@ cmp.setup({
 			"s",
 		}),
 	}),
-	formatting = {
-		-- fields = { "kind", "abbr", "menu" },
-		format = function(entry, vim_item)
-			vim_item.kind = kind_icons[vim_item.kind] .. " " .. vim_item.kind
-			vim_item.menu = ({
-				nvim_lsp = "",
-				nvim_lua = "",
-				luasnip = "",
-				buffer = "",
-				path = "",
-				emoji = "",
-			})[entry.source.name]
-			return vim_item
-		end,
-	},
 	sources = {
 		{ name = "nvim_lua" },
 		{ name = "nvim_lsp" },
