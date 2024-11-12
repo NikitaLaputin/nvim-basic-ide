5ead02b0dab600744a3025556d73f07d7f41b7fa
Mikita <mlaputsin@eisgroup.com>
2024-11-12 17:36:04 +0200

    Updated formatter

--- lua/user/lsp/conform.lua
+++ lua/user/lsp/conform.lua
@@ -1,13 +1,13 @@
 require("conform").setup({
 	formatters_by_ft = {
-		["lua"] = { "stylua" },
-		["python"] = { "isort", "black" },
-		["javascript"] = { "prettierd" },
-		["javascriptreact"] = { "prettierd" },
-		["typescript"] = { "prettierd" },
-		["typescriptreact"] = { "prettierd" },
-		["json"] = { "prettierd" },
-		["css"] = { "prettierd" },
-		["less"] = { "prettierd" },
+		lua = { "stylua" },
+		python = { "isort", "black" },
+		javascript = { "prettierd" },
+		javascriptreact = { "prettierd" },
+		typescript = { "prettierd" },
+		typescriptreact = { "prettierd" },
+		json = { "prettierd" },
+		css = { "prettierd" },
+		less = { "prettierd" },
 	},
 })
