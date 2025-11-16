dd3a044902fc2c426940655b0ccf13777a769f7f
Mikita <mlaputsin@eisgroup.com>
2025-11-16 14:30:21 +0200

    Added function to compare to clipboard

--- lua/user/helpers.lua
+++ lua/user/helpers.lua
@@ -57,4 +57,16 @@ M.open_line_commit_diff = function()
 	vim.cmd("DiffviewOpen " .. commit .. "^.." .. commit)
 end
 
+function _G.compare_to_clipboard()
+	local ftype = vim.api.nvim_eval("&filetype")
+	vim.cmd("vsplit")
+	vim.cmd("enew")
+	vim.cmd("normal! P")
+	vim.cmd("setlocal buftype=nowrite")
+	vim.cmd("set filetype="..ftype)
+	vim.cmd("diffthis")
+	vim.cmd([[execute "normal! \<C-w>h"]])
+	vim.cmd("diffthis")
+end
+
 return M
