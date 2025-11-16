808efd0e5aa93a809eb4e64f755508965385e627
Mikita <mlaputsin@eisgroup.com>
2025-11-16 14:25:09 +0200

    Added ts lsp

---
+++ lsp/typescript-language-server.lua
@@ -0,0 +1,24 @@
+---@type vim.lsp.Config
+return {
+	cmd = { "typescript-language-server", "--stdio" },
+	filetypes = {
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+	},
+    -- resolve starting from the current directory
+    root_markers = vim.fs.root(vim.env.PWD, { "tsconfig.json", "jsconfig.json", "package.json", ".git" }),
+
+	settings = {
+		typescript = {
+			preferences = {
+				includeCompletionsForModuleExports = true,
+				includeCompletionsForImportStatements = true,
+				importModuleSpecifier = "relative",
+			},
+		},
+	},
+}
